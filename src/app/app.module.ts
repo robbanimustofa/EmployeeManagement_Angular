b53f5e432180068d096f7b179193744827d9e5f8
robbanimustofa <robbanimustofa@gmail.com>
2023-03-10 09:59:43 +0700

    Make a component dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
 import { SingupComponent } from './singup/singup.component';
 import { TabViewModule } from 'primeng/tabview';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SingupComponent
+    SingupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

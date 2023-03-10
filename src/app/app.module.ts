6543dd994c23cc3dae45543c72ae4ebcd1ab65f7
robbanimustofa <robbanimustofa@gmail.com>
2023-03-10 08:13:58 +0700

    initial css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login/login.component';
+import { SingupComponent } from './singup/singup.component';
+import { TabViewModule } from 'primeng/tabview';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SingupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
